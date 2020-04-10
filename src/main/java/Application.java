3b165b3e10b1a7d8f483a1c0b7a316e64ba0d080
icyMojito <dev.icymojito@gmail.com>
2020-04-10 17:33:06 +0900

    feat : Application 클래스의 main 메서드에 결제하기 기능 구현 완료 및 리팩토링

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,70 +6,107 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        final List<Table> tables = TableRepository.tables();
+        final List<Menu> menus = MenuRepository.menus();
+
         while (true) {
             OutputView.printFeatures();
-            Feature feature;
-
-            while (true) {
-                try {
-                    feature = Feature.of(InputView.inputFeatureNumber());
-                    break;
-                } catch (IllegalArgumentException e) {
-                    OutputView.printError(e.getMessage());
-                }
-            }
+            Feature feature = createFeature();
 
             if (feature.isExit()) {
                 return;
             }
 
-            final List<Table> tables = TableRepository.tables();
             OutputView.printTables(tables);
-
-            final List<Menu> menus = MenuRepository.menus();
-
-            Table table;
-            while (true) {
-                try {
-                    String tableNumber = InputView.inputTableNumber();
-                    table = TableRepository.find(tableNumber);
-                    break;
-                } catch (IllegalArgumentException e) {
-                    OutputView.printError(e.getMessage());
-                }
-            }
-
-            OutputView.printMenus(menus);
+            Table table = findTable();
 
             if (feature.isOrder()) {
-                Menu menu;
-                while (true) {
-                    try {
-                        String menuNumber = InputView.inputMenuNumber();
-                        menu = MenuRepository.find(menuNumber);
-                        break;
-                    } catch (IllegalArgumentException e) {
-                        OutputView.printError(e.getMessage());
-                    }
-                }
-
-                MenuCount menuCount;
-                while (true) {
-                    try {
-                        menuCount = new MenuCount(InputView.inputMenuCount());
-                        break;
-                    } catch (IllegalArgumentException e) {
-                        OutputView.printError(e.getMessage());
-                    }
-                }
-
+                OutputView.printMenus(menus);
+                Menu menu = findMenu();
+                MenuCount menuCount = createMenuCount();
                 table.saveOrder(menu, menuCount);
-
-                continue;
             }
 
             if (feature.isPay()) {
+                OutputView.printOrders(table);
+                PayMethod payMethod = findPayMethod(table);
+
+                if (payMethod.isCash()) {
+                    OutputView.printCashDiscountedPrice(table);
+                } else {
+                    OutputView.printPrice(table);
+                }
+
+                table.completePay();
             }
         }
+    }
+
+    private static Feature createFeature() {
+        Feature feature;
+        while (true) {
+            try {
+                feature = Feature.of(InputView.inputFeatureNumber());
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e.getMessage());
+            }
+        }
+        return feature;
+    }
+
+    private static Table findTable() {
+        Table table;
+        while (true) {
+            try {
+                String tableNumber = InputView.inputTableNumber();
+                table = TableRepository.find(tableNumber);
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e.getMessage());
+            }
+        }
+        return table;
+    }
+
+    private static Menu findMenu() {
+        Menu menu;
+        while (true) {
+            try {
+                String menuNumber = InputView.inputMenuNumber();
+                menu = MenuRepository.find(menuNumber);
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e.getMessage());
+            }
+        }
+        return menu;
+    }
+
+    private static MenuCount createMenuCount() {
+        MenuCount menuCount;
+        while (true) {
+            try {
+                menuCount = new MenuCount(InputView.inputMenuCount());
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e.getMessage());
+            }
+        }
+        return menuCount;
+    }
+
+    private static PayMethod findPayMethod(Table table) {
+        PayMethod payMethod;
+        while (true) {
+            try {
+                OutputView.printPayMethod(table);
+                payMethod = PayMethod.of(InputView.inputPayMethodNumber());
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e.getMessage());
+            }
+        }
+        return payMethod;
     }
 }
