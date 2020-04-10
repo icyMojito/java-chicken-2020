8ae60fefdceff4e1729192789c15c9c86636fcec
icyMojito <dev.icymojito@gmail.com>
2020-04-10 16:54:28 +0900

    feat : 결제수단을 관리하는 PayMethod 클래스 생성

---
+++ src/main/java/domain/PayMethod.java
@@ -0,0 +1,37 @@
+package domain;
+
+import java.util.Arrays;
+
+import static util.NullValidator.validateNull;
+
+public enum PayMethod {
+    CARD("1"),
+    CASH("2");
+
+    private final String number;
+
+    PayMethod(String number) {
+        this.number = number;
+    }
+
+    public static PayMethod of(String input) {
+        validateNull(input);
+
+        return Arrays.stream(values())
+                .filter(v -> v.isEqualTo(input))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("> 잘못된 결제 수단 번호를 입력하였습니다."));
+    }
+
+    private boolean isEqualTo(String input) {
+        return this.number.equals(input);
+    }
+
+    public boolean isCard() {
+        return this == CARD;
+    }
+
+    public boolean isCash() {
+        return this == CASH;
+    }
+}
