9c992158789c698ceac48a455f9e94d9e29a42a4
icyMojito <dev.icymojito@gmail.com>
2020-04-10 16:27:51 +0900

    feat : Application 클래스의 main 메서드에 주문하기 기능 구현 추가 완료

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,66 +6,70 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        OutputView.printFeatures();
-        Feature feature;
-
         while (true) {
-            try {
-                feature = Feature.of(InputView.inputFeatureNumber());
-                break;
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e.getMessage());
-            }
-        }
+            OutputView.printFeatures();
+            Feature feature;
 
-        if (feature.isExit()) {
-            return;
-        }
-
-        final List<Table> tables = TableRepository.tables();
-        OutputView.printTables(tables);
-
-        final List<Menu> menus = MenuRepository.menus();
-
-        Table table;
-        while (true) {
-            try {
-                String tableNumber = InputView.inputTableNumber();
-                table = TableRepository.find(tableNumber);
-                break;
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e.getMessage());
-            }
-        }
-
-        OutputView.printMenus(menus);
-
-        if (feature.isOrder()) {
-            Menu menu;
             while (true) {
                 try {
-                    String menuNumber = InputView.inputMenuNumber();
-                    menu = MenuRepository.find(menuNumber);
+                    feature = Feature.of(InputView.inputFeatureNumber());
                     break;
                 } catch (IllegalArgumentException e) {
                     OutputView.printError(e.getMessage());
                 }
             }
 
-            MenuCount menuCount;
+            if (feature.isExit()) {
+                return;
+            }
+
+            final List<Table> tables = TableRepository.tables();
+            OutputView.printTables(tables);
+
+            final List<Menu> menus = MenuRepository.menus();
+
+            Table table;
             while (true) {
                 try {
-                    menuCount = new MenuCount(InputView.inputMenuCount());
+                    String tableNumber = InputView.inputTableNumber();
+                    table = TableRepository.find(tableNumber);
                     break;
                 } catch (IllegalArgumentException e) {
                     OutputView.printError(e.getMessage());
                 }
             }
 
-            table.saveOrder(menu, menuCount);
-        }
+            OutputView.printMenus(menus);
 
-        if (feature.isPay()) {
+            if (feature.isOrder()) {
+                Menu menu;
+                while (true) {
+                    try {
+                        String menuNumber = InputView.inputMenuNumber();
+                        menu = MenuRepository.find(menuNumber);
+                        break;
+                    } catch (IllegalArgumentException e) {
+                        OutputView.printError(e.getMessage());
+                    }
+                }
+
+                MenuCount menuCount;
+                while (true) {
+                    try {
+                        menuCount = new MenuCount(InputView.inputMenuCount());
+                        break;
+                    } catch (IllegalArgumentException e) {
+                        OutputView.printError(e.getMessage());
+                    }
+                }
+
+                table.saveOrder(menu, menuCount);
+
+                continue;
+            }
+
+            if (feature.isPay()) {
+            }
         }
     }
 }
